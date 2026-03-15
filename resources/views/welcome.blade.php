be6d51778f75ff211f0105032e2acc7dc666d0d2
bagusbatra <bagusbatr@gmail.com>
2026-03-15 15:25:45 +0700

    new commit

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,11 +4,11 @@
 		<title>BAGUS BATRA | Welcome</title>
 		<meta charset="utf-8" />
 		<meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=no" />
-		<link rel="stylesheet" href="assets/css/main.css" />
-        <link rel="icon" href="images/logo-black.png">
+		<link rel="stylesheet" href="home/assets/css/main.css" />
+        <link rel="icon" href="home/images/logo-black.png">
         <link rel="stylesheet" type="text/css" href="https://cdn.jsdelivr.net/npm/@phosphor-icons/web@2.1.1/src/regular/style.css"/>
         <link rel="stylesheet" type="text/css" href="https://cdn.jsdelivr.net/npm/@phosphor-icons/web@2.1.1/src/fill/style.css"/>
-		<noscript><link rel="stylesheet" href="assets/css/noscript.css" /></noscript>
+		<noscript><link rel="stylesheet" href="home/assets/css/noscript.css" /></noscript>
 	</head>
 	<body class="is-preload">
 
@@ -19,20 +19,20 @@
 					<header id="header">
 						<div class="logo">
 							<span class="icon">
-                                <img id="logoImage" src="images/logo-white.png" alt="">
+                                <img id="logoImage" src="home/images/logo-white.png" alt="">
                             </span>
 						</div>
 						<div class="content">
 							<div class="inner">
 								<h1>Welcome To <span class="typing">My Personal Web</span></h1>
-                                <p>This website is a place where I share <a href="">my projects</a>, <a href="">hobbies</a>, 
+                                <p>This website is a place where I share <a href="">my projects</a>, <a href="">activities</a>, 
                                     <a href="">experiences</a>, and <a href="">small moments</a> from my journey in technology and life. <br> 
                                     Feel free to explore my website and discover my projects and experiences.</p>
 							</div>
 						</div>
 						<nav>
 							<ul>
-								<li><a href="">About Me</a></li>
+								<li><a href="{{ route('aboutme') }}">About Me</a></li>
 								<li><a href="">Projects</a></li>
 								<li><a href="">Activities</a></li>
 								<li><a href="">Achievements</a></li>
@@ -44,7 +44,7 @@
 
 				<!-- Footer -->
 					<footer id="footer">
-						<p class="copyright"><b>&copy; 2026 Bagus. Thank you for visiting my personal website.</b></p>
+						<p class="copyright"><b>&copy; 2026 BagusBatra. Thank you for visiting my personal website.</b></p>
                         <a href=""><i class="ph ph-github-logo"></i></a>
                         <a href=""><i class="ph ph-linkedin-logo"></i></a>
                         <a href=""><i class="ph ph-whatsapp-logo"></i></a>
@@ -57,11 +57,11 @@
 			<div id="bg"></div>
 
 		<!-- Scripts -->
-			<script src="assets/js/jquery.min.js"></script>
-			<script src="assets/js/browser.min.js"></script>
-			<script src="assets/js/breakpoints.min.js"></script>
-			<script src="assets/js/util.js"></script>
-			<script src="assets/js/main.js"></script>
+			<script src="home/assets/js/jquery.min.js"></script>
+			<script src="home/assets/js/browser.min.js"></script>
+			<script src="home/assets/js/breakpoints.min.js"></script>
+			<script src="home/assets/js/util.js"></script>
+			<script src="home/assets/js/main.js"></script>
 
             <script>
             setTimeout(function() {
@@ -69,7 +69,7 @@
                 logo.style.opacity = "0";
 
                 setTimeout(function(){
-                    logo.src = "images/logo-real.png";
+                    logo.src = "home/images/logo-real.png";
                     logo.style.opacity = "1";
                 },500);
 
